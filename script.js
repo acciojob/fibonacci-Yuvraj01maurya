4095dfe0e6935bbda034e089e2624e54757575f0
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-11-18 22:02:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function fibonacci(num) {
 // your code here
+	function fibonacci(num) {
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let prevPrev = 0;
+    let prev = 1;
+    let current = 0;
+    for (let i = 2; i <= num; i++) {
+      current = prev + prevPrev;
+      prevPrev = prev;
+      prev = current;
+    }
+    return prevPrev;
+  }
+}
+
+// Test cases
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
+
 }
 
 module.exports = fibonacci;
